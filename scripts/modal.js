753ef2bc569862ddad051ab98a8530073f0aff5c
Yan Hong <Onlyhongyan@gmail.com>
2015-03-12 12:54:31 -0700

    add component modal

---
+++ scripts/modal.js
@@ -0,0 +1,8 @@
+function open_modal () {
+    var modal = document.getElementsByClassName('modal');
+    modal[0].style.display = "block";
+}
+function close_modal () {
+    var modal = document.getElementsByClassName('modal');
+    modal[0].style.display = "none";
+}
